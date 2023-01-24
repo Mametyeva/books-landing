9fb1f483e95c6d53d8751afd2ec05fa283850a22
Liza <eli.zaveta@bk.ru>
2023-01-24 15:46:19 +0700

    минус кнопка

--- script.js
+++ script.js
@@ -65,38 +65,3 @@ form.addEventListener('submit', function(evt) {
   list.appendChild(youTemplate);
   input.value = '';
 });
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// function newPosition (oldTop, oldLeft, width, height) {
-//     var range = 2; // Тут выставьте свой минимальный диапазон дальности
-//     var newTop = getRandomInt(0, height);
-//     var newLeft = getRandomInt(0, width);
-//     if ( ((oldTop-range)<newTop && (oldTop+range)>newTop) ||
-//          ((oldLeft-range)<newLeft && (oldLeft+range)>newLeft)) 
-//        return newPosition (oldTop, oldLeft, width, height);
-//     else return [newTop, newLeft];
-// }
-// $("#my-button").hover(function () {
-//     var buttonPosition = newPosition ( 
-//         parseInt($(this).css("top")), 
-//         parseInt($(this).css("left")), 
-//         $(".button-wrapper").width(), 
-//         $(".button-wrapper").height() 
-//     );
-//     $(this).css({
-//         top: buttonPosition[0],
-//         left: buttonPosition[1]
-//     });
-// }, function () { });
-
-
-
-// второй вариант
-$(document).on('mousemove', function (e) {
-    $('#my-button').css({
-        marginLeft: -e.pageX / 100 + 20,
-        marginTop: -e.pageY / 100 + 20
-    })
-})
